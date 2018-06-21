9cab7cb21efe3a5ba1edf1721413f15b9cb06191
alma19 <awashington@TC02MR1Z2FD56L.local>
2018-06-21 17:01:10 -0500

    Added images.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,50 +1,66 @@
 import $ from 'jquery';
-import 'selectize';
+import handlebars from 'handlebars';
+
 
 import './furniture';
 
+const HandlebarsIntl = require('handlebars-intl');
+
 $(document).ready(() => {
-  // <select id="select-repo"></select>
-  $('#select-repo').selectize({
-    valueField: 'url',
-    labelField: 'name',
-    searchField: 'name',
-    options: [],
-    create: false,
-    render: {
-      option: function(item, escape) {
-        return '<div>' +
-          '<span class="title">' +
-            '<span class="name"><i class="icon ' + (item.fork ? 'fork' : 'source') + '"></i>' + escape(item.name) + '</span>' +
-            '<span class="by">' + escape(item.username) + '</span>' +
-          '</span>' +
-          '<span class="description">' + escape(item.description) + '</span>' +
-          '<ul class="meta">' +
-            (item.language ? '<li class="language">' + escape(item.language) + '</li>' : '') +
-            '<li class="watchers"><span>' + escape(item.watchers) + '</span> watchers</li>' +
-            '<li class="forks"><span>' + escape(item.forks) + '</span> forks</li>' +
-          '</ul>' +
-        '</div>';
-      }
-    },
-    score: function(search) {
-      var score = this.getScoreFunction(search);
-      return function(item) {
-        return score(item) * (1 + Math.min(item.watchers / 100, 1));
-      };
-    },
-    load: function(query, callback) {
-      if (!query.length) return callback();
-      $.ajax({
-        url: 'https://api.github.com/legacy/repos/search/' + encodeURIComponent(query),
-        type: 'GET',
-        error: function() {
-          callback();
-        },
-        success: function(res) {
-          callback(res.repositories.slice(0, 10));
-        }
-      });
+  handlebars.registerHelper('namePossessive', (name) => {
+    const lastLetter = name.slice(-1);
+    if (lastLetter === 's' || lastLetter === 'z') {
+      return `${name}'`;
     }
+    return `${name}'s`;
+  });
+
+  HandlebarsIntl.registerWith(handlebars);
+
+  const cardSource = document.getElementById('ceo-card-template').innerHTML;
+  const cardTemplate = handlebars.compile(cardSource);
+
+  function drawCards(data) {
+    const ceos = data.splice(0, 100);
+    // 0,10
+    $.each(ceos, (k, v) => {
+      const cardHTML = cardTemplate(v);
+      $('.cards').append(cardHTML);
+
+      const medianemployees = v.employeeratio / 10;
+      const medianteachers = Math.round((v.ceopay / 52000) / 10);
+      const medianpolice = Math.round((v.ceopay / 42548) / 10);
+
+      // appending icons for median employee ratio
+      for (var i = 0; i < medianemployees; i++){
+        $(`<i class="fas fa-user"></i>`).appendTo($(`#card-${v.photo} .employee`));
+        // console.log(v.ceoname + ' ' + medianteachers);
+      }
+
+      // appending icons for median teachers ratio
+      for (var i = 0; i < medianteachers; i++){
+          $(`<i class="fas fa-user-graduate"></i>`).appendTo($(`#card-${v.photo} .teacher`));
+          // console.log(v.ceoname + ' ' + medianteachers);
+      }
+
+      for (var i = 0; i < medianpolice; i++){
+          $(`<i class="fas fa-user-shield"></i>`).appendTo($(`#card-${v.photo} .police`));
+        // console.log(v.ceoname + ' ' + medianpolice);
+      }
+
+
+
+      // first 10 div, rest are hidden
+
+      // give class for employee, teacher, police icons
+      // for each ratio, divide by 10. take that number and do for loop, append an icon to the div for every number. need to include photo id
+      // make sure it's appending to the div in the spefic card, not all of them
+    });
+  }
+  $.ajax({
+    url: 'https://interactives.dallasnews.com/data-store/2018/2018-06-ceo-pay-tracker.json',
+    cache: false,
+    success: drawCards,
+    dataType: 'json',
   });
 });
