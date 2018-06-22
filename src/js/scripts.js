9b8fa536b32cec3e7dffd92f099582784459f326
alma19 <awashington@TC02MR1Z2FD56L.local>
2018-06-22 10:15:55 -0500

    First 10 CEOs avaliable on start. Can click to see the rest.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,6 +7,13 @@ import './furniture';
 const HandlebarsIntl = require('handlebars-intl');
 
 $(document).ready(() => {
+  $('.hidden-cards').hide();
+
+  $('#showAll').click(function(){
+    $('.hidden-cards').show();
+  });
+
+
   handlebars.registerHelper('namePossessive', (name) => {
     const lastLetter = name.slice(-1);
     if (lastLetter === 's' || lastLetter === 'z') {
@@ -21,40 +28,72 @@ $(document).ready(() => {
   const cardTemplate = handlebars.compile(cardSource);
 
   function drawCards(data) {
-    const ceos = data.splice(0, 100);
-    // 0,10
-    $.each(ceos, (k, v) => {
+    const ceos = data;
+    const ceos10 = data.splice(0, 10);
+
+    console.log(ceos10);
+    console.log(ceos);
+
+    $.each(ceos10, (k, v) => {
       const cardHTML = cardTemplate(v);
       $('.cards').append(cardHTML);
 
+
+      // setting up the ratios
       const medianemployees = v.employeeratio / 10;
       const medianteachers = Math.round((v.ceopay / 52000) / 10);
       const medianpolice = Math.round((v.ceopay / 42548) / 10);
 
       // appending icons for median employee ratio
-      for (var i = 0; i < medianemployees; i++){
-        $(`<i class="fas fa-user"></i>`).appendTo($(`#card-${v.photo} .employee`));
+      for (let i = 0; i < medianemployees; i += 1) {
+        $('<i class="fas fa-user"></i>').appendTo($(`#card-${v.photo} .employee`));
         // console.log(v.ceoname + ' ' + medianteachers);
       }
 
       // appending icons for median teachers ratio
-      for (var i = 0; i < medianteachers; i++){
-          $(`<i class="fas fa-user-graduate"></i>`).appendTo($(`#card-${v.photo} .teacher`));
+      for (let i = 0; i < medianteachers; i += 1) {
+        $('<i class="fas fa-user-graduate"></i>').appendTo($(`#card-${v.photo} .teacher`));
           // console.log(v.ceoname + ' ' + medianteachers);
       }
 
-      for (var i = 0; i < medianpolice; i++){
-          $(`<i class="fas fa-user-shield"></i>`).appendTo($(`#card-${v.photo} .police`));
+      for (let i = 0; i < medianpolice; i += 1) {
+        $('<i class="fas fa-user-shield"></i>').appendTo($(`#card-${v.photo} .police`));
         // console.log(v.ceoname + ' ' + medianpolice);
       }
 
 
+      // first 10 div, rest are hidden
+    });
+
+    $.each(ceos, (k, v) => {
+      const cardHTML = cardTemplate(v);
+      $('.hidden-cards').append(cardHTML);
+
+
+      // setting up the ratios
+      const medianemployees = v.employeeratio / 10;
+      const medianteachers = Math.round((v.ceopay / 52000) / 10);
+      const medianpolice = Math.round((v.ceopay / 42548) / 10);
+
+      // appending icons for median employee ratio
+      for (let i = 0; i < medianemployees; i += 1) {
+        $('<i class="fas fa-user"></i>').appendTo($(`#card-${v.photo} .employee`));
+        // console.log(v.ceoname + ' ' + medianteachers);
+      }
+
+      // appending icons for median teachers ratio
+      for (let i = 0; i < medianteachers; i += 1) {
+        $('<i class="fas fa-user-graduate"></i>').appendTo($(`#card-${v.photo} .teacher`));
+          // console.log(v.ceoname + ' ' + medianteachers);
+      }
+
+      for (let i = 0; i < medianpolice; i += 1) {
+        $('<i class="fas fa-user-shield"></i>').appendTo($(`#card-${v.photo} .police`));
+        // console.log(v.ceoname + ' ' + medianpolice);
+      }
+
 
       // first 10 div, rest are hidden
-
-      // give class for employee, teacher, police icons
-      // for each ratio, divide by 10. take that number and do for loop, append an icon to the div for every number. need to include photo id
-      // make sure it's appending to the div in the spefic card, not all of them
     });
   }
   $.ajax({
