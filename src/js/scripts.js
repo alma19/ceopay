948b2dfd8d8993d425ed430f064ef724e1fed7bb
alma19 <awashi1919@gmail.com>
2018-06-26 09:21:29 -0500

    Working on filtering. Changing design to employee, teacher, and police bar graphs.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -12,17 +12,8 @@ $(document).ready(() => {
     $(this).addClass('no-show');
   });
 
-  // if (ceo.ceopay <= 1000000) {
-  //   console.log(ceo.ceopay + " ceo pay is less than 1 mil");
-  // } else {
-  //   $(`.card-${ceo.class}`).addClass('no-show');
-  //   console.log(ceo.ceopay + " ceo pay is more than 1 mil");
-  // }
 
 
-  // $('#ceo-sort').change(function(){
-  //
-  // })
 
   handlebars.registerHelper('namePossessive', (name) => {
     const lastLetter = name.slice(-1);
@@ -65,40 +56,81 @@ $(document).ready(() => {
     const medianteachers = Math.round((ceo.ceopay / 52000) / 10);
     const medianpolice = Math.round((ceo.ceopay / 42548) / 10);
 
-
-
-
-
-    // if (ceo.ceopay <= 1000000) {
-    //   console.log(ceo.ceopay + " ceo pay is less than 1 mil");
-    // } else {
-    //   $(`.card-${ceo.class}`).addClass('no-show');
-    //   console.log(ceo.ceopay + " ceo pay is more than 1 mil");
-    // }
-
       // the user clicks the dropdown
       // grab all the data between 0-1,000,00
       // show that data
       // hide everything
 
 
+    const oneMil = ceo.ceopay <= 1000000;
+    const fiveMil = ceo.ceopay >= 1000000 && ceo.ceopay <= 5000000;
+    const tenMil = ceo.ceopay >= 5000000 && ceo.ceopay <= 10000000;
+    const fifteenMil = ceo.ceopay >= 10000000 && ceo.ceopay <= 15000000;
+    const twentyMil = ceo.ceopay >= 15000000 && ceo.ceopay <= 20000000;
+
+    const ceoCompensation = [oneMil, fiveMil, tenMil, fifteenMil, twentyMil];
+
+
+    var filterActive;
+
+    function filterCategory(cat1, cat2, cat3){
+      // reset results publishDate
+      $('.filter-cat-results .f-cat').removeClass('active');
+
+      // the filtering in action for all criteria
+      let selector = '.filtering .f-cat';
+      if (cat1 !== 'cat-all'){
+        selector = '[data-cat=' + cat1 + ']';
+      }
+      if (cat2 !== 'cat-all'){
+        selector = selector + '[data-cat2=' + cat2 + ']';
+      }
+      if (cat3 !== 'cat-all') {
+        selector = selector + '[data-cat3=' + cat3 + "]";
+      }
+
+      // show all results
+      $(selector).addClass('active');
+
+      // reset active filter
+      filterActive = cat1;
+    }
+
+    // start by showing all items
+    $('.filtering .f-cat').addClass('active');
+
+    // call the filtering function when selects are changed
+    $('.filtering select').change(function(){
+      filterCategory($('.filtering select.cat1').val(), $('.filtering select.cat2').val(), $('.filtering select.cat3').val());
+    })
+
+    // for (let i = 0; i < 89; i += 1) {
+    //   if (twentyMil === false) {
+    //     $(`.card-${ceo.class}`).addClass('no-show');
+    //   }
+    // }
+    // $('select').change(function(){
+    //   let ceoFilter = $('#ceo-filter :selected').val();
+    //   console.log(ceoFilter);
+    //   console.log(ceoFilter === `${oneMil}`);
+    //   console.log(`"${  oneMil  }"`);
+    // });
 
 
     // appending icons for median employee ratio
     for (let i = 0; i < medianemployees; i += 1) {
-      $('<i class="fas fa-user"></i>').appendTo($(`#card-${ceo.id} .employee`));
+      $('<i class="fas fa-user"></i>').appendTo($(`#card-${ceo.class} .employee`));
     }
 
     // appending icons for median teachers ratio
     for (let i = 0; i < medianteachers; i += 1) {
-      $('<i class="fas fa-user-graduate"></i>').appendTo($(`#card-${ceo.id} .teacher`));
+      $('<i class="fas fa-user-graduate"></i>').appendTo($(`#card-${ceo.class} .teacher`));
     }
 
     for (let i = 0; i < medianpolice; i += 1) {
-      $('<i class="fas fa-user-shield"></i>').appendTo($(`#card-${ceo.id} .police`));
+      $('<i class="fas fa-user-shield"></i>').appendTo($(`#card-${ceo.class} .police`));
     }
-
-    };
+  }
 
 
   function drawCards(data) {
@@ -110,23 +142,31 @@ $(document).ready(() => {
     }
 
 
+
     const ceos = data;
     const ceos10 = data.splice(0, 10);
 
+    // for (let i = 0; i < ceos.length; i+=1){
+    //   const fifteenMil = ceos[i].ceopay <= 15000000;
+    //   console.log(ceos[i].company + ' is ' + fifteenMil + ' ' + ceos[i].ceopay);
+    // }
 
 
     $.each(ceos10, (k, v) => {
       const cardHTML = cardTemplate(v);
       $('.cards').append(cardHTML);
 
-      appendIcons(v);
+       appendIcons(v);
     });
 
     $.each(ceos, (k, v) => {
       const cardHTML = cardTemplate(v);
       $('.hidden-cards').append(cardHTML);
 
+
+
       appendIcons(v);
+
     });
   }
   $.ajax({
