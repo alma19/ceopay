c825c15bfc315675f3e5c8b6c6e7319c6eb04461
alma19 <awashi1919@gmail.com>
2018-06-27 09:38:19 -0500

    Made comparison bars instead of icons.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,12 +7,11 @@ import './furniture';
 const HandlebarsIntl = require('handlebars-intl');
 
 $(document).ready(() => {
+  $('.employee-compensation').css('width', '50%');
   $('#showAll').click(function () {
     $('.hidden-cards').removeClass('no-show');
     $(this).addClass('no-show');
   });
-
-
 
 
   handlebars.registerHelper('namePossessive', (name) => {
@@ -50,96 +49,48 @@ $(document).ready(() => {
   // let's use the DRY principle here and create a function that appends our icons
   // so we can use it in both places
 
-  function appendIcons(ceo) {
+  function appendIcons(data) {
     // setting up the ratios
-    const medianemployees = ceo.employeeratio / 10;
-    const medianteachers = Math.round((ceo.ceopay / 52000) / 10);
-    const medianpolice = Math.round((ceo.ceopay / 42548) / 10);
+    const medianemployees = data.employeeratio / 10;
+    const employeecompensation = (data.medianpay / maxEmployee ) * 100;
+    const teachercompensation = (52000 / maxEmployee) * 100;
+    const policecompensation = (65956 / maxEmployee) * 100;
 
-      // the user clicks the dropdown
-      // grab all the data between 0-1,000,00
-      // show that data
-      // hide everything
-
-
-    const oneMil = ceo.ceopay <= 1000000;
-    const fiveMil = ceo.ceopay >= 1000000 && ceo.ceopay <= 5000000;
-    const tenMil = ceo.ceopay >= 5000000 && ceo.ceopay <= 10000000;
-    const fifteenMil = ceo.ceopay >= 10000000 && ceo.ceopay <= 15000000;
-    const twentyMil = ceo.ceopay >= 15000000 && ceo.ceopay <= 20000000;
-
-    const ceoCompensation = [oneMil, fiveMil, tenMil, fifteenMil, twentyMil];
-
-
-    var filterActive;
-
-    function filterCategory(cat1, cat2, cat3){
-      // reset results publishDate
-      $('.filter-cat-results .f-cat').removeClass('active');
-
-      // the filtering in action for all criteria
-      let selector = '.filtering .f-cat';
-      if (cat1 !== 'cat-all'){
-        selector = '[data-cat=' + cat1 + ']';
-      }
-      if (cat2 !== 'cat-all'){
-        selector = selector + '[data-cat2=' + cat2 + ']';
-      }
-      if (cat3 !== 'cat-all') {
-        selector = selector + '[data-cat3=' + cat3 + "]";
-      }
-
-      // show all results
-      $(selector).addClass('active');
-
-      // reset active filter
-      filterActive = cat1;
-    }
-
-    // start by showing all items
-    $('.filtering .f-cat').addClass('active');
-
-    // call the filtering function when selects are changed
-    $('.filtering select').change(function(){
-      filterCategory($('.filtering select.cat1').val(), $('.filtering select.cat2').val(), $('.filtering select.cat3').val());
-    })
-
-    // for (let i = 0; i < 89; i += 1) {
-    //   if (twentyMil === false) {
-    //     $(`.card-${ceo.class}`).addClass('no-show');
-    //   }
-    // }
-    // $('select').change(function(){
-    //   let ceoFilter = $('#ceo-filter :selected').val();
-    //   console.log(ceoFilter);
-    //   console.log(ceoFilter === `${oneMil}`);
-    //   console.log(`"${  oneMil  }"`);
-    // });
-
-
-    // appending icons for median employee ratio
+    // apending handlebars
     for (let i = 0; i < medianemployees; i += 1) {
-      $('<i class="fas fa-user"></i>').appendTo($(`#card-${ceo.class} .employee`));
+      $('<i class="fas fa-user"></i>').appendTo($(`#card-${data.class} .employee`));
     }
 
-    // appending icons for median teachers ratio
-    for (let i = 0; i < medianteachers; i += 1) {
-      $('<i class="fas fa-user-graduate"></i>').appendTo($(`#card-${ceo.class} .teacher`));
-    }
 
-    for (let i = 0; i < medianpolice; i += 1) {
-      $('<i class="fas fa-user-shield"></i>').appendTo($(`#card-${ceo.class} .police`));
-    }
-  }
+    $(`#card-${data.class} .employee-bar`).css('width', `${employeecompensation}%`);
+
+    $(`#card-${data.class} .teacher-bar`).css('width', `${teachercompensation}%`);
+
+    $(`#card-${data.class} .police-bar`).css('width', `${policecompensation}%`);
 
 
-  function drawCards(data) {
-    // CHANGES
-    // lets give each row an ceo an id number that we use for the id on the card
+  } // function appendIcons
+
+  let maxEmployee = 0;
+
+  function findMax(data){
     for (let i = 0; i < data.length; i += 1) {
       // data[i].id = i;
       data[i].class = i;
+      if (data[i].medianpay >= maxEmployee) {
+        maxEmployee = data[i].medianpay;
+      }
     }
+    drawCards(data);
+    appendIcons(data);
+  }
+  function drawCards(data) {
+
+    // CHANGES
+    // lets give each row an ceo an id number that we use for the id on the card
+    let employeeCompensation;
+    const teacherCompensation = maxEmployee / 52000;
+    const policeCompensation = maxEmployee / 65956;
 
 
 
@@ -156,23 +107,20 @@ $(document).ready(() => {
       const cardHTML = cardTemplate(v);
       $('.cards').append(cardHTML);
 
-       appendIcons(v);
+      appendIcons(v);
     });
 
     $.each(ceos, (k, v) => {
       const cardHTML = cardTemplate(v);
       $('.hidden-cards').append(cardHTML);
 
-
-
       appendIcons(v);
-
     });
-  }
+  } // function drawCArds
   $.ajax({
     url: 'https://interactives.dallasnews.com/data-store/2018/2018-06-ceo-pay-tracker.json',
     cache: false,
-    success: drawCards,
+    success: findMax,
     dataType: 'json',
   });
 });
